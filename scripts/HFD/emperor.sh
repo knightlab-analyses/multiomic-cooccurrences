82338796c98a064bc6fdf72cd33d012855711e52
Jamie Morton <jamietmorton@gmail.com>
2019-04-04 13:22:29 -0400

    updating figures to reflect results in manuscript

--- scripts/HFD/emperor.sh
+++ scripts/HFD/emperor.sh
@@ -1,35 +1,35 @@
 # microbe pca plot
 qiime tools import --type "PCoAResults" \
-                   --input-path results/microbes-pca.results \
-                   --output-path results/microbes-pca.qza
+		   --input-path ../results/microbes-pca.results \
+		   --output-path ../results/hfd_output/microbes-pca.qza
 
-qiime emperor plot --i-pcoa results/microbes-pca.qza \
-                   --m-metadata-file data/tsne_microbe_feature_metadata.txt \
-                   --p-ignore-missing-samples \
-                   --o-visualization results/emperor-microbe-pca.qzv
+qiime emperor plot --i-pcoa ../results/hfd_output/microbes-pca.qza \
+		   --m-metadata-file ../../data/HFD/microbe_feature_metadata.txt \
+		   --p-ignore-missing-samples \
+		   --o-visualization ../results/hfd_output/emperor-microbe-pca.qzv
 
 
 # metabolite pca plot
-qiime tools import --type "PCoAResults" \
-                   --input-path results/metabolites-pca.results \
-                   --output-path results/metabolites-pca.qza
-
-qiime emperor plot --i-pcoa results/metabolites-pca.qza \
-                   --m-metadata-file data/tsne_metabolite_feature_metadata.txt \
-                   --p-ignore-missing-samples \
-                   --o-visualization results/emperor-metabolite-pca.qzv
-
+# qiime tools import --type "PCoAResults" \
+#                    --input-path results/metabolites-pca.results \
+#                    --output-path results/metabolites-pca.qza
+#
+# qiime emperor plot --i-pcoa results/metabolites-pca.qza \
+#                    --m-metadata-file data/tsne_metabolite_feature_metadata.txt \
+#                    --p-ignore-missing-samples \
+#                    --o-visualization results/emperor-metabolite-pca.qzv
+#
 
 
 # biplot
-# qiime tools import --type "PCoAResults % Properties('biplot')" \
-#                    --input-path results/omics-biplot.results  \
-#                    --output-path results/omics-biplot-results.qza
-# 
-# qiime emperor biplot --i-biplot results/omics-biplot-results.qza \
-#                      --m-sample-metadata-file data/biplot_metabolite_feature_metadata.txt \
-#                      --m-feature-metadata-file data/trimmed_microbe_feature_metadata.txt \
-#                      --p-ignore-missing-samples \
-#                      --p-number-of-features 100 \
-#                      --o-visualization results/emperor-mouse-omics.qzv
-# qiime tools view results/emperor-mouse-omics.qzv
+qiime tools import --type "PCoAResults % Properties('biplot')" \
+		   --input-path ../results/hfd_output/omics-biplot.results  \
+		   --output-path ../results/hfd_output/omics-biplot-results.qza
+
+qiime emperor biplot --i-biplot ../results/hfd_output/omics-biplot-results.qza \
+		     --m-sample-metadata-file ../data/HFD/biplot_metabolite_feature_metadata.txt \
+		     --m-feature-metadata-file ../data/HFD/microbe_feature_metadata.txt \
+		     --p-ignore-missing-samples \
+		     --p-number-of-features 100 \
+		     --o-visualization ../results/hfd_output/emperor-mouse-omics.qzv
+qiime tools view ../results/hfd_output/emperor-mouse-omics.qzv
--- scripts/HFD/songbird_run.sh
+++ scripts/HFD/songbird_run.sh
@@ -9,20 +9,16 @@
 #     --num-random-test-examples 10 \
 #     --beta-prior 0.01 \
 #     --summary-dir results/songbird_metabolites_prior0.01_lr1e-3_ABX_immune_status_diet
-    
 
 
 songbird multinomial \
-    --input-biom data/microbes.biom \
-    --formula 'HCC + diet' \
-    --metadata-file data/cleaned_qiime_metadata.txt \
+    --input-biom ../../data/HFD/microbes.biom \
+    --formula 'diet' \
+    --metadata-file ../data/HFD/cleaned_qiime_metadata.txt \
     --learning-rate 1e-3 \
     --batch-size 3 \
     --min-feature-count 20 \
-    --epoch 10000 \
-    --num-random-test-examples 20 \
-    --beta-prior 0.01 \
-    --summary-dir results/songbird_microbes_prior0.01_lr1e-3_HHC_diet
-    
-
-
+    --epochs 1000 \
+    --num-random-test-examples 10 \
+    --differential-prior 1 \
+    --summary-dir ../hfd_output/songbird_microbes_prior1_lr1e-3_diet_rerun3
